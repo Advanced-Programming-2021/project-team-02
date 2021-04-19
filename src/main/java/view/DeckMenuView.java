6870165aaa3abedfb2f80a49601e3cad1717160b
ErfanMojibi <mojibierfan@gmail.com>
2021-04-19 12:17:22 +0430

    some basics of DeckMenuView

--- src/main/java/view/DeckMenuView.java
+++ src/main/java/view/DeckMenuView.java
@@ -1,13 +1,18 @@
 package view;
 
+import controller.DeckMenuController;
 import view.input.Input;
+import view.input.Regex;
 import view.messages.Error;
 import view.messages.SuccessMessage;
 
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+
 public class DeckMenuView {
     private static DeckMenuView instance = null;
-
-    private DeckMenuView(){
+    private static final DeckMenuController controller = DeckMenuController.getInstance();
+    private DeckMenuView() {
 
     }
 
@@ -22,6 +27,32 @@ public class DeckMenuView {
     }
 
     public void commandRecognition(String command) {
+        Matcher matcher;
+        if ((matcher = Regex.getMatcher(Regex.DECK_CREATE, command)).matches()) {
+
+        } else if ((matcher = Regex.getMatcher(Regex.DECK_DELETE, command)).matches()) {
+
+        } else if ((matcher = Regex.getMatcher(Regex.DECK_SET_ACTIVATE, command)).matches()) {
+
+        } else if ((matcher = Regex.getMatcherFromAllPermutations(Regex.DECK_ADD_CARD_TO_MAIN_DECK, command)) != null) {
+
+        } else if ((matcher = Regex.getMatcherFromAllPermutations(Regex.DECK_ADD_CARD_TO_SIDE_DECK, command)) != null) {
+
+        } else if ((matcher = Regex.getMatcherFromAllPermutations(Regex.DECK_REMOVE_CARD_MAIN_DECK, command)) != null) {
+
+        } else if ((matcher = Regex.getMatcherFromAllPermutations(Regex.DECK_REMOVE_CARD_SIDE_DECK, command)) != null) {
+
+        } else if ((matcher = Regex.getMatcher(Regex.DECK_SHOW_ALL_CARDS, command)).matches()) {
+
+        } else if ((matcher = Regex.getMatcher(Regex.DECK_SHOW_MAIN_DECK, command)).matches()) {
+
+        } else if ((matcher = Regex.getMatcherFromAllPermutations(Regex.DECK_SHOW_SIDE_DECK, command)) != null){
+
+        } else if ((matcher = Regex.getMatcher(Regex.DECK_SHOW_ALL_CARDS,command)).matches()){
+
+        } else {
+            System.out.println("invalid command");
+        }
 
     }
 
@@ -31,4 +62,6 @@ public class DeckMenuView {
     public void showSuccessMessage(SuccessMessage message) {
 
     }
+
+
 }
