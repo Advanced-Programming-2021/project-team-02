6b19fdef5a57829f5862745b9b1d72dceb76a50b
ErfanMojibi <mojibierfan@gmail.com>
2021-05-14 16:30:43 +0430

    delete some comments

--- src/main/java/view/gameview/GameView.java
+++ src/main/java/view/gameview/GameView.java
@@ -217,42 +217,6 @@ public class GameView {
         return Input.getInput();
     }
 
-//    public Matcher getCardNameForChangeOfHeart() {
-//        System.out.println("please enter monster name and position for capturing this turn in this format: and then select card" +
-//                "card_name card_position(only OO or DO)");
-//        while (true) {
-//            String command = Input.getInput();
-//            Pattern pattern = Pattern.compile("([A-Za-z ',-]+?) (DO|OO)");
-//            Matcher matcher = pattern.matcher(command);
-//            if (command.equals("cancel")) return null;
-//            else if (matcher.matches()) return matcher;
-//            else System.out.println(Error.INVALID_COMMAND);
-//        }
-//    }
-
-//    public Matcher getCardsNameTwinTwister() {
-//        System.out.println("enter names in this format :" +
-//                "your_card_name opponent_card_name opponent_card_name");
-//        while (true) {
-//            String command = Input.getInput();
-//            Pattern pattern = Pattern.compile("([A-Za-z ',-]+?) ([A-Za-z ',-]+?) ([A-Za-z ',-]+?)");
-//            Matcher matcher = pattern.matcher(command);
-//            if (command.equals("cancel")) return null;
-//            if (matcher.matches()) return matcher;
-//            else System.out.println(Error.INVALID_COMMAND);
-//        }
-//    }
-
-//    public int mysticalSpaceTyphoon() {
-//        System.out.println("please enter address of spell or trap and then select card");
-//        while (true) {
-//            String command = Input.getInput();
-//            if (command.equals("cancel")) return -1;
-//            else if (command.matches("([12345])")) return Integer.parseInt(command);
-//            else System.out.println(Error.INVALID_COMMAND);
-//        }
-//    }
-
     public String swordOfDarkDestruction() {
         System.out.println("please enter card name (Card position should be OO or DO) and then select card:");
         while (true) {
@@ -272,38 +236,6 @@ public class GameView {
             else System.out.println(Error.INVALID_COMMAND);
         }
     }
-
-//    public String unitedWeStand() {
-//        System.out.println("please enter card name and then select card:");
-//        while (true) {
-//            String command = Input.getInput();
-//            if (command.equals("cancel")) return null;
-//            else if (command.matches("([A-Za-z ',-]+?)")) return command;
-//            else System.out.println(Error.INVALID_COMMAND);
-//        }
-//    }
-
-//    public String magnumShield() {
-//        System.out.println("please enter card name and then select card:");
-//        while (true) {
-//            String command = Input.getInput();
-//            if (command.equals("cancel")) return null;
-//            else if (command.matches("([A-Za-z ',-]+?)")) return command;
-//            else System.out.println(Error.INVALID_COMMAND);
-//        }
-//    }
-
-//    public String quickSpellsSwitchCase() {
-//        System.out.println("please enter card name in this format:" +
-//                "card_name");;
-//        String command;
-//        while (true) {
-//            command = Input.getInput();
-//            if (command.equals("cancel")) return null;
-//            else if (command.matches("([A-Za-z ',-]+?)")) return command;
-//            else System.out.println(Error.INVALID_COMMAND);
-//        }
-//    }
 
     public int getAddressForTrapOrSpell() {
         System.out.println("enter address(number of cell between 1 to 5) of spell to be activated or write cancel if you dont want to activate anything");
@@ -325,45 +257,11 @@ public class GameView {
         return false;
     }
 
-//    public String magicJammer() {
-//        System.out.println("enter card name to remove from your hand");
-//        while (true) {
-//            String command = Input.getInput();
-//            if (command.equals("cancel")) {
-//                return null;
-//            } else if (command.matches("([A-Za-z ',-]+?)")) {
-//                return command;
-//            } else System.out.println(Error.INVALID_COMMAND);
-//        }
-//    }
 
     public String askCardName() {
         return Input.getInput();
     }
 
-//    public int getNumberOfCardForCallOfTheHaunted() {
-//        System.out.println("enter number of the monster to be special summoned");
-//        while (true) {
-//            String command = Input.getInput();
-//            if (command.matches("\\d")) {
-//                return Integer.parseInt(command);
-//            } else System.out.println(Error.INVALID_COMMAND.getValue());
-//        }
-//    }
-//    public int howToSummonBeastKingBarbos(){
-//        System.out.println("how do you want to summon this card : " +
-//                "1-normal tribute" +
-//                ", 2-without tribute, 3-with 3 tributes\n" +
-//                "enter the numebr please");
-//        while (true){
-//            String input = Input.getInput();
-//            if (input.matches("[1-3]")){
-//                return Integer.parseInt(input);
-//            } else {
-//                System.out.println(Error.INVALID_COMMAND.getValue());
-//            }
-//        }
-//    }
     public int chooseCardInHand(){
         System.out.println("Enter address(number of it in your hand) of card to be set");
         String input;
@@ -372,6 +270,20 @@ public class GameView {
             if (input.matches("[1-9]+")){
                 return Integer.parseInt(input);
             } else System.out.println(Error.INVALID_COMMAND);
+        }
+    }
+    public int howToSummonBeastKingBarbos(){
+        System.out.println("how do you want to summon this card : " +
+                "1-normal tribute" +
+                ", 2-without tribute, 3-with 3 tributes\n" +
+                "enter the numebr please");
+        while (true){
+            String input = Input.getInput();
+            if (input.matches("[1-3]")){
+                return Integer.parseInt(input);
+            } else {
+                System.out.println(Error.INVALID_COMMAND.getValue());
+            }
         }
     }
 }
