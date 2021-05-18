da199e24bf0cad3140dccd833cb360cd26eda2d9
mhdsdt <mahdi.saadatbakht.1381@gmail.com>
2021-05-18 11:11:09 +0430

    write test for user create command and its errors

--- src/test/java/view/LoginMenuViewTest.java
+++ src/test/java/view/LoginMenuViewTest.java
@@ -1,6 +1,7 @@
 package view;
 
 import controller.LoginMenuController;
+import model.User;
 import model.card.CardsDatabase;
 import org.junit.jupiter.api.*;
 
@@ -20,25 +21,48 @@ class LoginMenuViewTest {
     }
 
     @Test
-    void run() {
-
-    }
-
-    @Test
-    void commandRecognition() {
-
-    }
-
-    @Test
-    void showDynamicError() {
-    }
-
-    @Test
-    void showCurrentMenu() {
+    @DisplayName ("Check \"user created successfully!\" message")
+    void checkUserCreatedSuccessfully() {
         ByteArrayOutputStream outContent = new ByteArrayOutputStream ();
         System.setOut (new PrintStream (outContent));
-        String expected = "Login Menu\n";
-        LoginMenuView.getInstance ().showCurrentMenu ();
+        String expected = "user created successfully!\n";
+        LoginMenuView.getInstance ().run ("user create --username mhdsdt --nickname mahdis --password ramz");
+        Assertions.assertEquals (expected, outContent.toString ());
+        Assertions.assertNotNull (User.getUserByUsername ("mhdsdt"));
+        Assertions.assertNotNull (User.getUserByNickName ("mahdis"));
+    }
+
+    @Test
+    @DisplayName ("Check \"user with username <username> already exists\" error")
+    void userAlreadyExistsWithDuplicateUsername() {
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream ();
+        System.setOut (new PrintStream (outContent));
+        System.setErr (new PrintStream (outContent));
+        String expected = "user created successfully!\nuser with username mhdsdt already exists\n";
+        LoginMenuView.getInstance ().run ("user create --username mhdsdt --password ramz --nickname mahdis");
+        LoginMenuView.getInstance ().run ("user create --nickname ali --password ramz --username mhdsdt");
+        Assertions.assertEquals (expected, outContent.toString ());
+    }
+
+    @Test
+    @DisplayName ("Check \"user with username <nickname> already exists\" error")
+    void userAlreadyExistsWithDuplicateNickname() {
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream ();
+        System.setOut (new PrintStream (outContent));
+        System.setErr (new PrintStream (outContent));
+        String expected = "user created successfully!\nuser with nickname mahdis already exists\n";
+        LoginMenuView.getInstance ().run ("user create -u mhdsdt -n mahdis -p ramz");
+        LoginMenuView.getInstance ().run ("user create -u ali -p ramz -n mahdis");
+        Assertions.assertEquals (expected, outContent.toString ());
+    }
+
+    @Test
+    @DisplayName ("Check \"please login first\" error")
+    void pleaseLoginFirst() {
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream ();
+        System.setErr (new PrintStream (outContent));
+        String expected = "please login first\n";
+        LoginMenuView.getInstance ().run ("menu enter Main");
         Assertions.assertEquals (expected, outContent.toString ());
     }
 
