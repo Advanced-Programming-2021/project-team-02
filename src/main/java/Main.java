be0262d03827e264b4a525429ee6f1fe5fab978a
mhdsdt <mahdi.saadatbakht.1381@gmail.com>
2021-06-07 14:33:29 +0430

    add some comment

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,9 @@ import view.MenusManager;
 
 import java.io.IOException;
 
+/**
+ * Salam da
+ */
 public class Main {
     public static void main(String[] args) throws IOException, CloneNotSupportedException {
         CardsDatabase.getInstance().readAndMakeCards();
