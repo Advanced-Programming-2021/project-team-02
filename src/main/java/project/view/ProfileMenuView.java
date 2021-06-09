f71eacd0ff0f4e9f36aae08b5fd270767b9843ee
lover1975 <drjoenh1983@gmail.com>
2021-06-09 03:52:27 -0700

    kharabe faghat maven darim khoda ghovat

--- src/main/java/project/view/ProfileMenuView.java
+++ src/main/java/project/view/ProfileMenuView.java
@@ -22,7 +22,7 @@ public class ProfileMenuView extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         stage = primaryStage;
-        URL urlMain = getClass().getResource("project/fxml/ProfileMenuView.fxml");
+        URL urlMain = getClass().getResource("/project/fxml/ProfileMenuView.fxml");
         System.out.println(urlMain);
         Parent root = FXMLLoader.load(Objects.requireNonNull(urlMain));
         primaryStage.setScene(new Scene(root));
@@ -36,48 +36,48 @@ public class ProfileMenuView extends Application {
         return instance;
     }
 
-    public void run(String command) {
-        commandRecognition(command);
-    }
+//    public void run(String command) {
+//        commandRecognition(command);
+//    }
+//
+//    public void commandRecognition(String command) {
+//        Matcher matcher;
+//        if ((matcher = Regex.getMatcher (Regex.MENU_ENTER, command)).matches ()) {
+//            if (matcher.group ("menuName").toLowerCase(Locale.ROOT).equals ("profile")) {}
+////                showDynamicError (Error.BEING_ON_CURRENT_MENU, Menu.PROFILE_MENU.getValue ());
+//            else Error.showError (Error.BEING_ON_A_MENU);
+//        } else if (Regex.getMatcher (Regex.MENU_EXIT, command).matches ()) {
+////            MenusManager.getInstance().changeMenu(Menu.MAIN_MENU);
+//        } else if ((Regex.getMatcher (Regex.MENU_SHOW_CURRENT, command)).matches ()) {
+//            showCurrentMenu ();
+//        } else if ((matcher = Regex.getMatcher (Regex.PROFILE_CHANGE_NICKNAME, command)).matches ()) {
+//            controller.changeNickname (matcher.group ("nickname"));
+//        } else if ((matcher = Regex.getMatcherFromAllPermutations (Regex.PROFILE_CHANGE_PASSWORD, command)) != null) {
+//            controller.changePassword (matcher.group ("currentPassword"), matcher.group ("newPassword"));
+//        } else if (Regex.getMatcher (Regex.COMMAND_HELP, command).matches ()) {
+//            help ();
+//        } else Error.showError (Error.INVALID_COMMAND);
+//    }
 
-    public void commandRecognition(String command) {
-        Matcher matcher;
-        if ((matcher = Regex.getMatcher (Regex.MENU_ENTER, command)).matches ()) {
-            if (matcher.group ("menuName").toLowerCase(Locale.ROOT).equals ("profile")) {}
-//                showDynamicError (Error.BEING_ON_CURRENT_MENU, Menu.PROFILE_MENU.getValue ());
-            else Error.showError (Error.BEING_ON_A_MENU);
-        } else if (Regex.getMatcher (Regex.MENU_EXIT, command).matches ()) {
-//            MenusManager.getInstance().changeMenu(Menu.MAIN_MENU);
-        } else if ((Regex.getMatcher (Regex.MENU_SHOW_CURRENT, command)).matches ()) {
-            showCurrentMenu ();
-        } else if ((matcher = Regex.getMatcher (Regex.PROFILE_CHANGE_NICKNAME, command)).matches ()) {
-            controller.changeNickname (matcher.group ("nickname"));
-        } else if ((matcher = Regex.getMatcherFromAllPermutations (Regex.PROFILE_CHANGE_PASSWORD, command)) != null) {
-            controller.changePassword (matcher.group ("currentPassword"), matcher.group ("newPassword"));
-        } else if (Regex.getMatcher (Regex.COMMAND_HELP, command).matches ()) {
-            help ();
-        } else Error.showError (Error.INVALID_COMMAND);
-    }
+//    public void showDynamicError(Error error, String string) {
+//        if (error.equals (Error.TAKEN_USERNAME))
+//            System.out.printf (Error.TAKEN_USERNAME.getValue (), string);
+//        else if (error.equals (Error.TAKEN_NICKNAME))
+//            System.out.printf (Error.TAKEN_NICKNAME.getValue (), string);
+//        else if (error.equals (Error.BEING_ON_CURRENT_MENU))
+//            System.out.printf (error.getValue (), string);
+//    }
 
-    public void showDynamicError(Error error, String string) {
-        if (error.equals (Error.TAKEN_USERNAME))
-            System.out.printf (Error.TAKEN_USERNAME.getValue (), string);
-        else if (error.equals (Error.TAKEN_NICKNAME))
-            System.out.printf (Error.TAKEN_NICKNAME.getValue (), string);
-        else if (error.equals (Error.BEING_ON_CURRENT_MENU))
-            System.out.printf (error.getValue (), string);
-    }
+//    public void showCurrentMenu() {
+//        System.out.println ("Profile Menu");
+//    }
 
-    public void showCurrentMenu() {
-        System.out.println ("Profile Menu");
-    }
-
-    public void help() {
-        System.out.println ("menu show-current\n" +
-                "profile change --nickname <nickname>\n" +
-                "profile change --password --current <currentPassword> --new <newPassword>\n" +
-                "profile change -p -c <currentPassword> -n <newPassword>\n" +
-                "menu exit\n" +
-                "help");
-    }
+//    public void help() {
+//        System.out.println ("menu show-current\n" +
+//                "profile change --nickname <nickname>\n" +
+//                "profile change --password --current <currentPassword> --new <newPassword>\n" +
+//                "profile change -p -c <currentPassword> -n <newPassword>\n" +
+//                "menu exit\n" +
+//                "help");
+//    }
 }
