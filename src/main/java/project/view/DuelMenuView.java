7b2dab71459ecb55bc95200f77824608b8edbf5a
ErfanMojibi <mojibierfan@gmail.com>
2021-07-02 17:46:42 +0430

    start duel appearance finished

--- src/main/java/project/view/DuelMenuView.java
+++ src/main/java/project/view/DuelMenuView.java
@@ -5,6 +5,8 @@ import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
@@ -26,15 +28,18 @@ public class DuelMenuView {
 
     }
 
-    public void openSinglePlayerChoices(ActionEvent actionEvent) {
-
+    public void openSinglePlayerChoices(MouseEvent actionEvent) {
+        if (actionEvent.getButton() != MouseButton.PRIMARY)
+            return;
         Button oneRoundButton = new Button("One Round Duel");
         final StartDuelMessage[] message = new StartDuelMessage[1];
         oneRoundButton.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent actionEvent) {
                 message[0] = DuelMenuController.getInstance().startDuelWithAI(1);
-                new PopUpMessage(message[0].getAlertType(), message[0].getLabel());
+                if (message[0] != StartDuelMessage.SUCCESS)
+                    new PopUpMessage(message[0].getAlertType(), message[0].getLabel());
+                //TODO else start game
             }
         });
         Button matchButton = new Button("Duel Match");
@@ -42,21 +47,28 @@ public class DuelMenuView {
             @Override
             public void handle(ActionEvent actionEvent) {
                 message[0] = DuelMenuController.getInstance().startDuelWithAI(3);
-                new PopUpMessage(message[0].getAlertType(), message[0].getLabel());
+                if (message[0] != StartDuelMessage.SUCCESS)
+                    new PopUpMessage(message[0].getAlertType(), message[0].getLabel());
+                //TODO else start game
+
             }
         });
         VBox box = new VBox(oneRoundButton, matchButton);
+        box.getStylesheets().add(Objects.requireNonNull(getClass().getResource("/project/CSS/duel_start_vbox.css")).toExternalForm());
         box.setLayoutX(singlePlayerButton.getLayoutX() + 200);
         box.setLayoutY(singlePlayerButton.getLayoutY());
         pane.getChildren().add(box);
 
     }
 
-    public void openMultiPlayerChoices(ActionEvent actionEvent) {
-
+    public void openMultiPlayerChoices(MouseEvent actionEvent) {
+        if (actionEvent.getButton() != MouseButton.PRIMARY)
+            return;
     }
 
-    public void back(ActionEvent actionEvent) throws IOException {
+    public void back(MouseEvent actionEvent) throws IOException {
+        if (actionEvent.getButton() != MouseButton.PRIMARY)
+            return;
         Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/project/fxml/main_menu.fxml")));
         Utility.openNewMenu(root, (Node) actionEvent.getSource());
     }
